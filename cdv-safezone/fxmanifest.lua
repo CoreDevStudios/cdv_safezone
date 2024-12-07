d1bdb6fb5eb3c441160337c5aede7ea56550f025
CoreDevStudios <nitrogamer975@gmail.com>
2024-12-07 17:52:34 +0500

    Add files via upload

---
+++ cdv-safezone/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CodeDevStudio'
+description 'Safezone Script for FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
